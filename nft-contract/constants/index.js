0841398c815dd4915595903b35395c510e6df273
Lario <osunkunleolanrewaju@gmail.com>
2023-01-25 16:30:26 +0100

    wrote and deployed the nft-collection contract

---
+++ nft-contract/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5bBe3C7A5351077eCEc268Dc4bc9Df04Fc7AcE42";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
